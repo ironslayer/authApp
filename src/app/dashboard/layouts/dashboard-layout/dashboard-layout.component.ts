f55d5917bfbd48b1ffd8e5b8f3d5856ea42a91b1
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-21 18:20:27 -0400

    feat(auth): implement authentication module with login and register pages
    - Added AuthRoutingModule to manage routes for login and register pages.
    - Created AuthLayoutComponent to serve as a layout for authentication-related views.
    - Developed LoginPageComponent and RegisterPageComponent with basic templates.
    - Introduced AuthService for future authentication logic.
    - Added unit tests for all new components.
    
    feat(dashboard): set up dashboard module with layout component
    
    - Created DashboardRoutingModule for dashboard routing.
    - Implemented DashboardLayoutComponent as the main layout for the dashboard.
    - Added basic template for dashboard layout.
    - Included unit tests for the dashboard layout component.

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+
+}
